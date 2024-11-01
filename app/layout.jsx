821dd7d33c7e645a1b70569266d276becaac3afb
ifirmawan <firmawaneiwan@gmail.com>
2024-11-01 15:29:16 +0700

    feat: Implement ReCaptcha provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { ReCaptchaProvider } from 'next-recaptcha-v3';
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
@@ -16,13 +17,15 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.svg" sizes="any" />
       </head>
       <body className="antialiased text-white bg-blue-900">
-        <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-          <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-            <Header />
-            <div className="grow">{children}</div>
-            <Footer />
+        <ReCaptchaProvider>
+          <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
+            <div className="flex flex-col w-full max-w-5xl mx-auto grow">
+              <Header />
+              <div className="grow">{children}</div>
+              <Footer />
+            </div>
           </div>
-        </div>
+        </ReCaptchaProvider>
       </body>
     </html>
   );
