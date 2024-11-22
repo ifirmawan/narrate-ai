45585519c3b0dd399d5752b59665a7b6bd6603aa
ifirmawan <firmawaneiwan@gmail.com>
2024-11-22 15:57:44 +0700

    fix: Update prompts

--- actions/mistral-ai.js
+++ actions/mistral-ai.js
@@ -4,13 +4,13 @@ import { Mistral } from '@mistralai/mistralai';
 export const createStory = (story) =>
   new Promise(async (resolve, reject) => {
     try {
-      let prompt =
-        "I'd like you to act as a professional blogger, spelling corrector, and editor.";
-      prompt +=
-        "I will tell you a story in any language, and you'll enhance, expand, and correct it to make it more elegant, polished, and expressive, using refined language and advanced vocabulary.";
-      prompt += `Please respond using the following JSON structure: { "output": "string", "created_at": "current_time", "duration": "time" }. Avoid formatting in Markdown and respond in plain JSON only.
-`;
-      prompt += `Here is my story: <<${story}>>.`;
+      const prompts = [
+        "I'd like you to act as a professional blogger, editor, and spelling corrector.",
+        'I will share a story in any language, and your task will be to enhance, expand, and refine it into a comprehensive blog post of at least three paragraphs, using polished language and advanced vocabulary.',
+        `Please respond using the following JSON structure: { "output": "string", "created_at": "current_time", "duration": "time" }.`,
+        `Avoid formatting in Markdown and respond in plain JSON only. Here is my story: <<${story}>>.`
+      ];
+      const prompt = prompts.join('');
       const apiKey = process.env.MISTRAL_API_KEY;
       const client = new Mistral({ apiKey: apiKey });
       const chatResponse = await client.chat.complete({
